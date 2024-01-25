54b8a42e3ee189421cc92b24b5681cf4aa1af055
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-01-25 08:22:27 -0500

    fresh start

--- src/components/MenuButton.jsx
+++
@@ -1,6 +0,0 @@
-export default function MenuButton({children}) {
-
-  return (
-    <button className="menu--button">{children}</button>
-  )
-}
--- src/components/MenuItem.jsx
+++
@@ -1,6 +0,0 @@
-export default function MenuItem({children}) {
-  
-  return (
-    <div className="menu--item">{children}</div>
-  )
-}
